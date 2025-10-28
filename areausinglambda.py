cf91023a2e292dd1f9e4caa2eee96e7bee80165e
mdhv-7 <chn25mca041@ceconline.edu>
2025-10-28 13:52:48 +0530

    Add files via upload

---
+++ areausinglambda.py
@@ -0,0 +1,23 @@
+
+sqs = int(input("Enter the side of the square: "))
+
+
+print("Enter base and height of the triangle:")
+ht = int(input("Height: "))
+bs = int(input("Base: "))
+
+
+print("Enter length and breadth of the rectangle:")
+l = int(input("Length: "))
+b = int(input("Breadth: "))
+
+
+square_area = sqs * sqs
+triangle_area = 0.5 * bs * ht
+rectangle_area = l * b
+
+
+print("Area of the square:", square_area)
+print("Area of the triangle:", triangle_area)
+print("Area of the rectangle:", rectangle_area)
+
